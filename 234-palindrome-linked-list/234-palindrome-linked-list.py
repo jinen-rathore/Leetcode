d761e06e17ea27128163e62d3a506d4a7845d058
Jinen Rathore <jinenrathore@gmail.com>
2023-01-11 00:36:22 +0530

    Time: 796 ms (86.90%), Space: 38.9 MB (91.14%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -7,10 +7,40 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         # method 1
         # put the linked list into array and solve the problem
-        arr = []
+#         arr = []
         
-        while head:
-            arr.append(head.val)
-            head = head.next
+#         while head:
+#             arr.append(head.val)
+#             head = head.next
+        
+#         l,r = 0, len(arr) - 1
+#         while l <= r:
+#             if arr[l] != arr[r]:
+#                 return False
+#         return True
+        # return arr == arr[::-1]
+        
+#         Method 2
+        
+        #finding mid point
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
             
-        return arr == arr[::-1]
+        #reversing the second half
+        
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
