4bfbcf9b469bd433be2fd3c7b9c0f93fd8fb418e
Jinen Rathore <jinenrathore@gmail.com>
2023-02-07 12:52:12 +0530

    Time: 41 ms (48.37%), Space: 13.9 MB (23.64%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,21 +3,39 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        left, right = 0, len(matrix) - 1
+        # method 1
+#         left, right = 0, len(matrix) - 1
         
+#         while left < right:
+#             for i in range(right - left):
+#                 top, bottom = left, right 
+                
+#                 topLeft = matrix[top][left+i]
+            
+#                 matrix[top][left+i] = matrix[bottom-i][left]
+            
+#                 matrix[bottom-i][left] = matrix[bottom][right-i]
+            
+#                 matrix[bottom][right-i] = matrix[top+i][right]
+            
+#                 matrix[top+i][right] = topLeft
+                
+#             left += 1
+#             right -= 1
+
+        # Method 2
+        left = 0
+        right = len(matrix) - 1
+        
+        # revers the matrix
         while left < right:
-            for i in range(right - left):
-                top, bottom = left, right 
-                
-                topLeft = matrix[top][left+i]
-            
-                matrix[top][left+i] = matrix[bottom-i][left]
-            
-                matrix[bottom-i][left] = matrix[bottom][right-i]
-            
-                matrix[bottom][right-i] = matrix[top+i][right]
-            
-                matrix[top+i][right] = topLeft
-                
-            left += 1
+            matrix[left], matrix[right] = matrix[right], matrix[left]
             right -= 1
+            left += 1
+            
+        # transpose the matrix
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+                
