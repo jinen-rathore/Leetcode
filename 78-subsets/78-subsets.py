99595106764c9516df17b59dcfdd7fcf453fdb73
Jinen Rathore <jinenrathore@gmail.com>
2023-02-07 11:01:41 +0530

    Time: 40 ms (48.50%), Space: 13.9 MB (99.60%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,16 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [[]]
+#         if len(nums) == 0:
+#             return [[]]
         
-        subset = []
+#         subset = []
         
-        first_ele = nums[0]
-        remaining_ele = nums[1:]
+#         first_ele = nums[0]
+#         remaining_ele = nums[1:]
         
-        for partial_subset in self.subsets(remaining_ele):
-            subset.append(partial_subset)
-            subset.append(partial_subset + [first_ele])
-        return subset
+#         for partial_subset in self.subsets(remaining_ele):
+#             subset.append(partial_subset)
+#             subset.append(partial_subset + [first_ele])
+#         return subset
+
+        # Approach 2
+        ret = [[]]
+        for n in nums:
+            ret += [r + [n] for r in ret]
+        return ret
         
         
