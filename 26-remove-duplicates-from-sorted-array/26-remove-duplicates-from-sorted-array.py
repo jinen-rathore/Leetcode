0ff72ea11c6df26fc1bbfd303e5dd5784e495907
Jinen Rathore <jinenrathore@gmail.com>
2023-05-05 18:03:50 +0530

    Time: 107 ms (24.23%), Space: 17.8 MB (11.24%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -1,3 +1,16 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        nums[:] = sorted(list(set(nums)))
+        # creating 2 pointers one left starting from 0 index and one right strarting from 1
+        l, r = 0, 1
+        
+        # till right >= left and r not equal to end
+        while l <= r and r != len(nums):
+            
+            # if the numbers are not equal we need to swap the location 
+            if nums[l] != nums[r]:
+                nums[l + 1] = nums[r]
+                l += 1
+            # if they are same just keep scaning till we get a mismatch and then swap with the next element of left pointer 
+            else:
+                r += 1
+        return l + 1
