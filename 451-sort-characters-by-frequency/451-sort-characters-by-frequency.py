3d10389a8f886fa2c5596d85611f2e45de732e28
Jinen Rathore <jinenrathore@gmail.com>
2023-05-08 20:16:54 +0530

    Time: 63 ms (26.48%), Space: 17.7 MB (18.20%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,13 +1,17 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
+        # creating a hashmap and storing the frequencies 
         d = {}
         for c in s:
             if c in d:
                 d[c] += 1
             else:
                 d[c] = 1
+                
+        # reverse sorting the formed dictionary
         d =  dict(sorted(d.items(), key=operator.itemgetter(1),reverse=True))
         
+        # appending the output to empty string as the number of frequency of occuring 
         output = ""
         for key, val in d.items():
             output += key*val
