7f24b7c008887e03f1aac458fa0413b839cda16e
Jinen Rathore <jinenrathore@gmail.com>
2023-08-19 22:31:32 +0530

    Time: 34 ms (97.59%), Space: 16.7 MB (25.58%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,11 +1,17 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        arr = [x+1 for x in range(n)]
-        m = 0
-        
-        while len(arr) > 1:
-            m = (m + k - 1) % len(arr)
-            arr.pop(m)
-        return arr[0]
+        # for the recursive relation we can find the next element if we know
+        # the prev by adding k
+        # f(n) = f(n-1) + k
+        # we do the mod to keep in range of n because we are sitting in a round table
+        # f(n) = (f(n-1) + k) % n
+        def helper(n, k):
+            if n == 1:
+                return 0
+            return (helper(n-1, k) + k) % n
+        # we do +1 in the end to convert 0 indexing to 1 based indexing 
+        return helper(n, k) + 1
+
+
             
             
