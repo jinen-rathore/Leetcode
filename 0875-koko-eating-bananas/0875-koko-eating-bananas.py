4fdef552b1a2612aaf387a29e095517c42c766b0
Jinen Rathore <jinenrathore@gmail.com>
2023-10-01 11:16:52 +0530

    Time: 409 ms (14.30%), Space: 17.8 MB (68.40%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,16 +2,25 @@ import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # for the optimal approach we will be using binary search
+        
+        # we go from 1 to the max values in the plies list
         low, high = 1, max(piles)
         while low <= high:
+            # calculating mid value
             mid = (low + high) // 2
             
+            # so we need to find the total number of hours koko takes to eat all the bananas
+            # we can get that by dividing each vales of the piles list by the mid value
+            # then adding them up
             req_hours = 0
             for i in range(len(piles)):
                 req_hours += math.ceil(piles[i] / mid)
             
+            # if the hours is less than or equal to h:
+            # h is a candidate and we can find h so we eliminate the right part of the numbers
             if req_hours <= h:
                 high = mid - 1
+            # else we eliminate the left part of numbers
             else:
                 low = mid + 1
         return low
