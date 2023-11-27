3da978cc178b2500857669938fc884f5a38729f0
Jinen Rathore <jinenrathore@gmail.com>
2023-11-27 22:47:00 +0530

    Time: 32 ms (91.80%), Space: 16.3 MB (5.65%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -39,8 +39,5 @@ class Solution:
         
         if n == 0:
             return False
-        if n & (n-1) == 0:
-            return True
-        else:
-            return False
+        return n & (n-1) == 0
         
