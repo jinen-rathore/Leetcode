e91fc692da490ed2a56307eb694a6852cdf13cf1
Jinen Rathore <jinenrathore@gmail.com>
2023-11-28 19:52:31 +0530

    Time: 137 ms (23.92%), Space: 17.6 MB (58.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -8,8 +8,22 @@ class Solution:
                 return i
         return len(nums)
         """
-        
+        """
         # approach 2:
         n = len(nums)
         return n*(n+1)//2 - sum(nums)
+        """
         
+        # approach 3 using XOR
+        res = 0
+        for i in range(len(nums) + 1): # creating the full list ex for [3,0,1] full list is [0,1,2,3]
+            res ^= i
+        
+        for i in nums:
+            # we xor with the given list of numbers
+            # the similar numbers with the xor present will cancel 
+            # ex [0,1,2,3] ex => res = 0 ^ 1 ^ 2 ^ 3
+            # in this step res = 0 ^ 1 ^ 2 ^ 3 ^ 0 0and0 will cancel
+            # in the end only 2 will be left unpaired
+            res ^= i
+        return res
