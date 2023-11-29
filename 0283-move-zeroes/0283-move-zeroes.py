6d29842b6f4db8fa520be7969990fad0210635f4
Jinen Rathore <jinenrathore@gmail.com>
2023-11-29 08:30:32 +0530

    Time: 142 ms (74.61%), Space: 18 MB (20.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i, j = 0,0
-        while i < len(nums) and j < len(nums):
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-            j += 1
+        # i, j = 0,0
+        # while i < len(nums) and j < len(nums):
+        #     if nums[j] != 0:
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         i += 1
+        #     j += 1
+        
+        # using snowball method
+        ss = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                ss += 1
+            elif ss > 0:
+                # swap with the left most snowball
+                nums[i - ss] = nums[i]
+                nums[i] = 0
+        return nums
         
         
