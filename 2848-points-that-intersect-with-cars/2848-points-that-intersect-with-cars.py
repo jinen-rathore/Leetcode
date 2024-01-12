91d66209d373c11e41145e75ab038b4296560007
Jinen Rathore <jinenrathore@gmail.com>
2024-01-12 23:35:23 +0530

    Time: 83 ms (54.13%), Space: 17.3 MB (16.21%) - LeetHub

---
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        h = [0] * 101
+        for s,e in nums:
+            for i in range(s, e+1):
+                h[i] = 1
+        count = 0
+        for i in h:
+            if i == 1:
+                count += 1
+        return count
