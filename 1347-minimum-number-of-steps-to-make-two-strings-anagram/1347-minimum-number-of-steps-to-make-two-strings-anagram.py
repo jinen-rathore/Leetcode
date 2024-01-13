b4d7bad1c503d7f07f3216a9e4eec7e0d5abbeba
Jinen Rathore <jinenrathore@gmail.com>
2024-01-13 09:22:55 +0530

    Time: 219 ms (25.81%), Space: 18 MB (5.24%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,22 +1,10 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        h1,h2 = {}, {}
-        for i in s:
-            if i not in h1:
-                h1[i] = 1
-            else:
-                h1[i] += 1
-        for i in t:
-            if i not in h2:
-                h2[i] = 1
-            else:
-                h2[i] += 1
-        
+        h = collections.Counter(s)        
         res = 0
-        for k, v in h2.items():
-            if k not in h1:
-                res += v
+        for char in t:
+            if h[char] and h[char] > 0:
+                h[char] -= 1
             else:
-                if v > h1[k]:
-                    res += v - h1[k]
+                res += 1
         return res
