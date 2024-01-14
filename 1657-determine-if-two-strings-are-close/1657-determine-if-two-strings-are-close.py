92d55863e9b1edb0c2e3dd7f50f0b5bf510cb7a6
Jinen Rathore <jinenrathore@gmail.com>
2024-01-14 11:03:16 +0530

    Time: 166 ms (53.10%), Space: 18.5 MB (20.97%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,7 +1,15 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        # for this approach we use double hashmap
+        # first we hash the frequency of all the elements in both the words
+        # then we hash the frequence of all the elements
+        
+        # because we can arrange all the words freely we just need to check that desired frequency 
+        # is present and we can change it accordingly
+        
         if set(word1) != set(word2):
             return False
+        
         
         h1,h2 = {},{}
         
@@ -15,7 +23,8 @@ class Solution:
                 h2[c] += 1
             else:
                 h2[c] = 1
-                
+
+
         n1,n2 = {}, {}
         
         for i in h1.values():
@@ -28,5 +37,4 @@ class Solution:
                 n2[i] += 1
             else:
                 n2[i] = 1
-        
         return n1 == n2
