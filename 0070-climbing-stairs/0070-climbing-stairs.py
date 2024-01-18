75383d25fc20bf03ebfb8c5fee283e7a1da84083
Jinen Rathore <jinenrathore@gmail.com>
2024-01-18 08:27:06 +0530

    Time: 37 ms (59.30%), Space: 17.4 MB (15.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        # Time complexity: O(N)
+        # We will use dp as to get to nth step if we get the (n-1)th step and (n-2)th step we can add those and get the answer
+        # so essentily it converts to a fibb series problem
+        if n == 1:
+            return 1
+        # using tabulation
+        dp = [0] * (n+1)
+        
+        for i in range(2,n+1):
+            # we are the the first step => number is 1 
+            dp[0] = 1
+            # similarly to jump to second we only need one step
+            dp[1] = 1
+            # after that to get to any other step we need 
+            # the sum on previous 2 steps as we can only jump 1 or 2 steps
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
