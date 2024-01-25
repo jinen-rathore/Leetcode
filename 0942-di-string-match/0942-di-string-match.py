4f5dd1c608f754c34bd650d86e33293433ad1bd1
Jinen Rathore <jinenrathore@gmail.com>
2024-01-25 13:20:43 +0530

    Time: 58 ms (50.25%), Space: 17.8 MB (60.76%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.py
+++ 0942-di-string-match/0942-di-string-match.py
@@ -2,23 +2,14 @@ class Solution:
     def diStringMatch(self, s: str) -> List[int]:
         res = []
         l, r = 0, len(s)
-        if s[0] == "D":
-            res.append(r)
-            r -= 1
-        else:
-            res.append(l)
-            l += 1
-            
-        for i in range(1, len(s)):
-            if s[i] == "I":
+        
+        for char in s:
+            if char == "I":
                 res.append(l)
                 l += 1
-            elif s[i] == "D":
+            else:
                 res.append(r)
                 r -= 1
-        if s[-1] == "I":
-            res.append(l)
-        else:
-            res.append(r)
+        res.append(r)
         return res
         
