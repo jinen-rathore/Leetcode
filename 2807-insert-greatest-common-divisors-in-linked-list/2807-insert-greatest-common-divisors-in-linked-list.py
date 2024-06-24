4061e2221c47f0644f1a91e348fec96235462841
Jinen Rathore <jinenrathore@gmail.com>
2024-06-24 16:04:58 +0530

    Time: 77 ms (31.01%), Space: 19.6 MB (63.36%) - LeetHub

---
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def findGCD(a,b):
+            if b == 0:
+                return a
+            return findGCD(b, a%b)
+        curr = head
+        
+        while curr and curr.next:
+            curr.next = ListNode(findGCD(curr.val, curr.next.val), next = curr.next)
+            curr = curr.next.next
+        return head
+            
+            
+            
+            
