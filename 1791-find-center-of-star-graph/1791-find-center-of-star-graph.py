6a2b9d443ce8d346172d7dba07cd3c2d4b75beab
Jinen Rathore <jinenrathore@gmail.com>
2024-06-27 10:01:49 +0530

    Time: 654 ms (66.44%), Space: 52.1 MB (86.58%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -36,9 +36,6 @@ class Solution:
         
         e1, e2 = edges[0], edges[1]
         
-        if e1[0] in e2:
-            return e1[0]
-        else:
-            return e1[1]
+        return e1[0] if e1[0] in e2 else e1[1]
         
             
