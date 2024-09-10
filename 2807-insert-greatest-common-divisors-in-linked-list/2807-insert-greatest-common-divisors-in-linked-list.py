4da00011d3ad0bf1907529cb4bf2fd3f7a73cb8e
Jinen Rathore <jinenrathore@gmail.com>
2024-09-10 18:12:20 +0530

    Time: 88 ms (19.45%), Space: 19.7 MB (10.35%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.py
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.py
@@ -5,13 +5,14 @@
 #         self.next = next
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        # Finding GCD 
         def findGCD(a,b):
             if b == 0:
                 return a
             return findGCD(b, a%b)
-        curr = head
         
+        curr = head
+        # creating a node and inserting in a linked list as usual
         while curr and curr.next:
             curr.next = ListNode(findGCD(curr.val, curr.next.val), next = curr.next)
             curr = curr.next.next
